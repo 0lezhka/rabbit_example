074f190d55c61c3ee195176cdebd03e94ff9dbea
Oleh Kopylchak <oleg12311999@gmail.com>
2020-01-15 13:27:04 +0200

    init commit

---
+++ src/test/java/com/kopylchak/rabbit_example/RabbitExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kopylchak.rabbit_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
